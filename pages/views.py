88f5c267d4075ac9e4e03f68d25af94dcdb9df37
daniel2234 <kwiatkowski.daniel@gmail.com>
2022-07-30 15:37:03 -0400

    implement routes and testing

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-from django.http import HttpResponse
 # Create your views here.
+class HomePageView(TemplateView):
+  template_name = "home.html"
 
-def homePageView(request):
-  return HttpResponse("Hello, World!")
+class AboutPageView(TemplateView):
+  template_name = "about.html"
